e89724cdcbf087f06c2eebdf5e5996e98670e988
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-12 10:13:14 +0530

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,12 @@
+# Exercise 1
+
+#add a person with name Tony Stark, his age is 51 years and he is a genius
+
+first_name="Tony"
+last_name="Stark"
+age=51
+is_genius=True
+
+print( first_name+" "+last_name)
+print( age)
+print(is_genius)
