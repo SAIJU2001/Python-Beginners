8bbe246540d0cf9f248e0fc63da2a092630e338f
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-12 10:12:38 +0530

    Add files via upload

---
+++ arithmeticOperatorInPython.py
@@ -0,0 +1,11 @@
+#arithmetic operator in python
+
+first=input("Enter first number : ")
+second=input("Enter second number : ")
+print("addition is : "+str( int(first)+int(second) ))
+print("subtraction is : "+str( int(first)-int(second) ))
+print("multiplication is : "+str( int(first)*int(second) ))
+print("division is : "+str( int(first)/int(second) ))
+print("modulus is : "+str( int(first)%int(second) ))
+print("to the power is : "+str( int(first)**int(second) ))
+
