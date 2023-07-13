121b8998544e7e1371711bee06ce9190c88ba09d
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-13 09:37:25 +0530

    Add files via upload

---
+++ breakAndContinue.py
@@ -0,0 +1,15 @@
+#break and continue
+
+students=["ram","shyam","kishan","radha","radhika"]
+
+#break statement
+for student in students :
+    if student=="radha" :
+        break;
+    print(student)
+
+#continue statement
+for student in students :
+    if student=="radha" :
+        continue;
+    print(student)
