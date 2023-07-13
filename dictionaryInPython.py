644f1aa3d6981ab8f1cf8cc0546d863eb85b16cc
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-13 09:37:51 +0530

    Add files via upload

---
+++ dictionaryInPython.py
@@ -0,0 +1,16 @@
+#dictionary in python
+
+#key and value is present(like hashmap)
+marks={"english" : 95,"chemistry" : 97, "math" : 99}
+print(marks)
+
+#print any specific value using key
+print(marks["chemistry"])
+
+#to add new marks
+marks["physics"]=96
+print(marks)
+
+#to change existing marks
+marks["math"]=100
+print(marks)
