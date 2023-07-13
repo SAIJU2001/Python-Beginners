bae72948a73e312bf5344618eb877a1ed20b5865
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-13 09:38:12 +0530

    Add files via upload

---
+++ ifElseUsingPython.py
@@ -0,0 +1,13 @@
+#if else using python
+
+age=input("Enter your age : ")
+age=int(age)
+if age>=18 :
+    print("You are adult")
+    print("You can vote")
+elif age<18 and age>=6 :  
+    print("You are in school")
+else :
+    print("You are a child")
+print("Thank You")
+
