0510f8f033ab3b1fb728c95a6e0d7194738c31c4
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-15 09:32:15 +0530

    Add files via upload

---
+++ stringInPython.py
@@ -0,0 +1,21 @@
+#string in python
+
+name=input("Enter your name : ")
+print(name)
+
+#uppercase the string
+print(name.upper())
+
+#lowercase the string
+print(name.lower())
+
+#find index of any character or string 
+find=input("Enter your char or string : ")
+print(name.find(find))
+
+#replace any character or string 
+new_name=input("Enter your char or string : ")
+print(name.replace(name,new_name))
+ 
+
+
