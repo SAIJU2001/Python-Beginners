bb55c718b8454789da3dec9a6186b59f9923062f
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-16 13:15:52 +0530

    Add files via upload

---
+++ typeConversion.py
@@ -0,0 +1,18 @@
+#type conversion
+
+age=input("Enter your age : ")
+print(age)
+print(type(age))
+
+#convert it into int
+new_age=int(age)
+print(new_age)
+print(type(new_age))
+
+#convert it into float
+
+recent_age=float(age)
+print(recent_age)
+print(type(recent_age))
+
+#other type casting also available bool(),str()
