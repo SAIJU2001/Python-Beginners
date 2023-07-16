9109fc8d61bf08f34aecd3cd08271267a0363e09
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-16 13:15:14 +0530

    Add files via upload

---
+++ sumOfTwoNumbers.py
@@ -0,0 +1,7 @@
+#to calculate the sum of two numbers
+
+
+val1=input("Enter your first Value : ")
+val2=input("Enter your Second Value : ")
+sum=int(val1)+int(val2)
+print("Sum of "+val1+" and "+val2+" is : "+str(sum))
