3213c8777dd8392e58bc1bf31dd3bdac9ec8b333
Saikat Naskar <102968768+SAIJU2001@users.noreply.github.com>
2023-07-16 13:16:25 +0530

    Add files via upload

---
+++ variablesInPython.py
@@ -0,0 +1,14 @@
+#variables in python
+
+#here we do not define variable type just write the name and assign the value
+
+name="saikat"
+age=22
+print(name)
+print(age)
+
+# we can overwrite this variable too
+
+name="sani"
+print(name)
+print(age)
